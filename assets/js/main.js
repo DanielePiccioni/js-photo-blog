7885426a4723e7e8c35dd5fcea0e685029555fee
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-27 12:09:25 +0100

    create a const for api to put easly the call ajax or fetch and take other const for container to put the other card in future

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+//const per far richiamare APi molto piu semplicemente
+const api = "https://lanciweb.github.io/demo/api/pictures/";
+//const per richiamare l'elemento container dall'html per poi inserirci le altre card
+const container = document.getElementById("container");
