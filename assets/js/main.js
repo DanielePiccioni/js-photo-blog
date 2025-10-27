cc762c00f1bbd9171c2c7824c205b19d66d2cec3
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-27 18:40:33 +0100

    stamp on html all photo not only one with forEach

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
 //const per far richiamare APi molto piu semplicemente
 const api = "https://lanciweb.github.io/demo/api/pictures/";
+
 //const per richiamare l'elemento container dall'html per poi inserirci le altre card
 const container = document.getElementById("photo-container");
-
 
 //chiamata API con fetch
 fetch(api)
     .then(response => response.json())
     .then(data => {
         console.log(data.length);
-        //prende solamente una foto
-        const photo = data[0];
-        const col = document.createElement("div");
-        col.className = "col-12 col-sm-6 col-md-4";
-        col.innerHTML = `
+        // svuoto il contenitore prima di inserire le card
+        container.innerHTML = "";
+        // ciclo tutte le foto
+        data.forEach(photo => {
+            const col = document.createElement("div");
+            col.className = "col-12 col-sm-6 col-md-4";
+            col.innerHTML = `
     <div class="card shadow position-relative">
       <div class="puntina"></div>
       <img src="${photo.url}" class="w-100" alt="${photo.title}">
@@ -22,5 +24,6 @@ fetch(api)
         <p class="card-text fst-italic small">${photo.title}</p>
       </div>
     </div>`
-        container.appendChild(col);
+            container.appendChild(col);
+        });
     })
